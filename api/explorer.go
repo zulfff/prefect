69395b9bc442c69f28f99b4300277f4593d25de7
daniswastaken <daniswastaken@gmail.com>
2026-02-03 21:29:06 +0700

    feat: implement FileExplorer API to retrieve file, sidebar, and drives data

--- api/explorer.go
+++ api/explorer.go
@@ -1,1 +1,43 @@
 package api
+
+import (
+	"net/http"
+	"prefect/services/parser"
+	"prefect/services/file"
+	"encoding/json"
+)
+
+type FileExplorer struct {
+	Files   []file.FilesList   `json:"files"`
+	Sidebar []parser.SidebarData `json:"sidebar"`
+	Drives  []parser.DrivesData  `json:"drives"`
+}
+
+func FileExplorerAPI(response http.ResponseWriter, request *http.Request) {
+	filesData, err := parser.FileEntriesParser()
+	if err != nil {
+		http.Error(response, "Failed to get file entries", http.StatusInternalServerError)
+		return
+	}
+
+	sidebarData, err := parser.SidebarDataParser()
+	if err != nil {
+		http.Error(response, "Failed to get sidebar data", http.StatusInternalServerError)
+		return
+	}
+
+	drivesData, err := parser.DrivesDataParser()
+	if err != nil {
+		http.Error(response, "Failed to get drives data", http.StatusInternalServerError)
+		return
+	}
+
+	fileExplorerResponse := FileExplorer{
+		Files:   filesData,
+		Sidebar: sidebarData,
+		Drives:  drivesData,
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(fileExplorerResponse)
+}
