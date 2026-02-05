84ea4c5de75bca9e577e391ad502eaef8c5a532e
daniswastaken <daniswastaken@gmail.com>
2026-02-05 09:55:21 +0700

    delete folder and file function

--- services/file/operations.go
+++ services/file/operations.go
@@ -8,6 +8,7 @@ import (
 
 var root, _ = os.UserHomeDir()
 
+// Renaming file function
 func RenameFile(path string, newName string) error {
 	// New file name can't be empty
 	if newName == "" || strings.Contains(newName, "/") {
@@ -39,4 +40,54 @@ func RenameFile(path string, newName string) error {
 
 	// Perform the rename operation
 	return os.Rename(absPath, newPath)
+}
+
+// Delete file function
+func DeleteFile(path string) error {
+	// File must live inside root
+	cleanPath := filepath.Clean(path)
+
+	absPath := filepath.Join(root, cleanPath)
+
+	relPath, err := filepath.Rel(root, absPath)
+	if err != nil {
+		return os.ErrPermission
+	}
+
+	if strings.HasPrefix(relPath, "..") {
+		return os.ErrPermission
+	}
+
+	// Is the destination exist?
+	if _, err := os.Stat(absPath); err != nil {
+		return os.ErrNotExist
+	}
+
+	// Perform the delete operation
+	return os.Remove(absPath)
+}
+
+// Delete folder function
+func DeleteFolder(path string) error {
+	// File must live inside root
+	cleanPath := filepath.Clean(path)
+
+	absPath := filepath.Join(root, cleanPath)
+
+	relPath, err := filepath.Rel(root, absPath)
+	if err != nil {
+		return os.ErrPermission
+	}
+
+	if relPath == "." || strings.HasPrefix(relPath, "..") {
+		return os.ErrPermission
+	}
+
+	// Is the destination exist?
+	if _, err := os.Stat(absPath); err != nil {
+		return os.ErrNotExist
+	}
+
+	// Perform the delete operation
+	return os.RemoveAll(absPath)
 }
