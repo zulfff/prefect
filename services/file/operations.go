8a971f9ba3a9cee0ffd5a9c843cb994988ff95b5
daniswastaken <daniswastaken@gmail.com>
2026-02-05 13:37:03 +0700

    add CopyFile function for file operations

--- services/file/operations.go
+++ services/file/operations.go
@@ -1,22 +1,23 @@
 package file
 
 import (
+	"io"
 	"os"
-	"strings"
 	"path/filepath"
+	"strings"
 )
 
 var root, _ = os.UserHomeDir()
 
 // Renaming file function
-func RenameFile(path string, newName string) error {
+func RenameFile(sourcePath string, newName string) error {
 	// New file name can't be empty
 	if newName == "" || strings.Contains(newName, "/") {
 		return os.ErrInvalid
 	}
 
 	// File must live inside root
-	cleanPath := filepath.Clean(path)
+	cleanPath := filepath.Clean(sourcePath)
 
 	absPath := filepath.Join(root, cleanPath)
 
@@ -90,4 +91,68 @@ func DeleteFolder(path string) error {
 
 	// Perform the delete operation
 	return os.RemoveAll(absPath)
+}
+
+// Copy file function
+func CopyFile(sourcePath string, destinationDir string) error {
+	// Clean the paths
+	cleanSourcePath := filepath.Clean(sourcePath)
+	cleanDestinationDir := filepath.Clean(destinationDir)
+
+	// Construct absolute paths
+	destinationDirLocation := filepath.Join(root, cleanDestinationDir)
+	sourceLocation := filepath.Join(root, cleanSourcePath)
+
+	// Ensure source and destination are within root
+	fileRelPath, err := filepath.Rel(root, sourceLocation)
+	if err != nil || strings.HasPrefix(fileRelPath, "..") {
+		return os.ErrPermission
+	}
+
+	desRelPath, err := filepath.Rel(root, destinationDirLocation)
+	if err != nil || strings.HasPrefix(desRelPath, "..") {
+		return os.ErrPermission
+	}
+
+	destinationFile := filepath.Join(destinationDirLocation, filepath.Base(sourcePath))
+
+	// Check source file existence and is it not a directory?
+	if f, err := os.Stat(sourceLocation); err != nil {
+		return os.ErrNotExist
+	} else if f.IsDir() {
+		return os.ErrInvalid
+	}
+
+	// Check the destination file and is it a directory?
+	if _, err := os.Stat(destinationFile); err == nil {
+		return os.ErrExist
+	}
+
+	info, err := os.Stat(destinationDirLocation)
+	if err != nil {
+		return os.ErrNotExist
+	}
+	if !info.IsDir() {
+		return os.ErrInvalid
+	}
+
+	// Perform the copy operation
+	srcFile, err := os.Open(sourceLocation)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(destinationFile)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
